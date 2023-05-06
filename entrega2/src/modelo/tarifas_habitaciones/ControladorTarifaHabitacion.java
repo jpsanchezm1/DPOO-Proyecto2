ce3260dc294e311b851573217ec6ff4da11bd7d4
LexMarcus27 <98420957+LexMarcus27@users.noreply.github.com>
2023-05-06 17:09:38 -0500

    Tarifas_Calendario

--- entrega2/src/modelo/tarifas_habitaciones/ControladorTarifaHabitacion.java
+++ entrega2/src/modelo/tarifas_habitaciones/ControladorTarifaHabitacion.java
@@ -1,63 +1,30 @@
 package modelo.tarifas_habitaciones;
 
-import modelo.utilidades.Utilidades;
-
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class ControladorTarifaHabitacion {
-
-	private Float precioActual;
-	private CargadorTarifaHabitacion cargadorTarifas;
+	
+	private CalendarioTarifasHab calendarioTarifas;
 
 	public ControladorTarifaHabitacion() {
-		this.cargadorTarifas = new CargadorTarifaHabitacion();
+		calendarioTarifas = new CalendarioTarifasHab();
 	}
 
 	public void cargarTarifas(String rutaArchivo) throws IOException {
-		cargadorTarifas.cargarTarifas(rutaArchivo);
+		CargadorTarifaHabitacion cargadorTarifas = new CargadorTarifaHabitacion();
+		cargadorTarifas.cargarTarifas(rutaArchivo,calendarioTarifas);
 	}
 
-	private Float consultarTarifaHabitacion(String tipoHabitacion, String fechaString) {
-		Map<String, Map<String, Map<String, Float>>> mapaTipos = this.cargadorTarifas.getMapaTarifas();
-		Map<String, Map<String, Float>> mapaTarifas = mapaTipos.get(tipoHabitacion);
-		Set<String> rangos = mapaTarifas.keySet();
-		Iterator<String> rangosIterator = rangos.iterator();
-		LocalDate fecha = LocalDate.parse(fechaString);
-		Float precio = null;
-		boolean tarifaHallada = false;
-		while (rangosIterator.hasNext() && !tarifaHallada) {
-			String rango = rangosIterator.next();
-			String[] fechas = rango.split(";"); // rango es de la forma: "fechaInicio;fechaFin"
-			LocalDate fechaInicio = LocalDate.parse(fechas[0]);
-			LocalDate fechaFin = LocalDate.parse(fechas[1]);
-			boolean fechaContenida = fecha.isAfter(fechaInicio) && fecha.isBefore(fechaFin);
-			if (fechaContenida || rango.contains(fechaString)) {
-				String diaDeLaSemana = Utilidades.getDiaEnEspaniol(fecha.getDayOfWeek());
-				precio = mapaTarifas.get(rango).get(diaDeLaSemana);
-				tarifaHallada = true;
-			}
-		}
-		return precio;
-	}
-
-	public void consultarTarifaHabitacion(String tipoHabitacion, String fechaInicioString, String fechaFinString) {
+	public float consultarTarifaHabitacion(String tipoHabitacion, String fechaInicioString, String fechaFinString) {
 		LocalDate fechaInicio = LocalDate.parse(fechaInicioString);
 		LocalDate fechaFin = LocalDate.parse(fechaFinString);
 		Float precio = 0f;
 		LocalDate fecha = fechaInicio;
 		while (fecha.isBefore(fechaFin) || fecha.equals(fechaFin)) {
-			precio += consultarTarifaHabitacion(tipoHabitacion, fecha.toString());
+			precio += calendarioTarifas.consultarTarifaHabitacion(tipoHabitacion, fecha.toString());
 			fecha = fecha.plusDays(1);
 		}
-		precioActual = precio;
-	}
-
-	public Float getPrecioActual() {
-		return precioActual;
+		return precio;
 	}
 }
