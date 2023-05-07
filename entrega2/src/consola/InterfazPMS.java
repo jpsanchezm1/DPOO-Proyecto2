2e89cce3fbb3100dbaf7b402c86ed133a5bb4524
ahenaog <111033675+ahenaog@users.noreply.github.com>
2023-05-07 10:00:44 -0500

    Interfaz consola (#12)
    * borrar interfaz por consola
    
    * inicio interfaz

---
+++ entrega2/src/consola/InterfazPMS.java
@@ -0,0 +1,30 @@
+package consola;
+
+import javax.swing.JFrame;
+
+public class InterfazPMS extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Inicio panelInicio;
+	
+	public InterfazPMS() {
+
+		setTitle("Property Managament System");
+		setSize(700, 600);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		panelInicio = new Inicio();
+		
+		add(panelInicio);
+	}
+
+	public static void main(String[] args) {
+		InterfazPMS interfaz = new InterfazPMS();
+		interfaz.setVisible(true);
+	}
+}
